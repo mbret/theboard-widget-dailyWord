e418708787b22028764b03f0cce9d41769920845
Maxime Bret <bret.maxime@gmail.com>
2015-09-07 01:19:25 +0200

    rewrite

--- index.js
+++ index.js
@@ -1,65 +1,29 @@
-/**
- * Created by Damien on 25/01/2015.
- */
-
 (function(){
 
-    /**
-     * Widget definition
-     *
-     * ==============
-     * Your widget
-     * ==============
-     * Your widget must implement init() method. Otherwise the widget will not be handle by library.
-     *
-     * Your widget can be build in the way you want. It's however recommended to follow this structure
-     *
-     * widget.start()   => This method start your widget
-     * widget.refresh()   => This method refresh your widget
-     * widget.stop()    => This method stop your widget
-     * ...
-     *
-     * Once your widget is build, the library will call init() method and pass data.
-     *
-     * ==============
-     * WidgetAdapter
-     * ==============
-     * A WidgetAdapter is also passed through window by the library
-     * WidgetAdapter contain some info and more important the settings your widget asked
-     * Example:
-     *  WidgetAdapter.signal    => Contain the current signal
-     *  WidgetAdapter.widget    => Contain all your widget configuration
-     *  WidgetAdapter.widget.settings  => Contain informations you asked
-     *  WidgetAdapter.widget.identity  => Contain your widget identity
-     *  ...
-     *
-     */
-    window.Widget = {
+    var Widget = {
 
-        //identity: foo // filled by library
-        configuration: null,
+        configuration: {},
 
         // This method is call by the library
         init: function( conf ){
             this.configuration = conf;
-            WidgetUtils.log.debug(this.identity + ' is initializing');
             this.initDisplay();
             this.start();
         },
+
         // This method will be call when widget should be start
         start: function(){
-            WidgetUtils.log.debug(this.identity + ' is running');
             this.refreshProcess.start();
         },
+
         // ...
         stop: function(){
-            WidgetUtils.log.debug(this.identity + ' is stopped');
             this.displayStopped();
             this.refreshProcess.stop();
         },
+
         // ...
         refresh: function(){
-            WidgetUtils.log.debug(this.identity + ' is refreshing');
             this.displayRefreshing();
             this.refreshProcess.refresh();
         },
@@ -94,19 +58,22 @@
         refreshFunction: function(){
             // do some stuff (get info from google API ?)
             // ...
-            setTimeout(this.displayContent, 1000);
+            setTimeout(this.displayContent.bind(this), 1000);
         },
 
         initDisplay: function(){
             $('.widget-header').html('<h1>'+this.configuration.identity+'</h1>');
-            $('.widget-content').html(
-                '<p>Widget is loading</p>'
-            );
         },
 
         displayContent: function(){
-            Widget.getDailyWord();
-            $('.widget-updated').html('Updated: ' + new Date().toISOString());
+            this.getDailyQuote()
+                .then(function(data){
+                    console.log(data);
+                    $('#quote-title').html(data.title);
+                    $('#quote-content').html(data.content);
+                    $('.widget-updated').html('Updated: ' + new Date().toISOString());
+                    $('#content-loading').hide();
+                });
         },
 
         displayError: function(){
@@ -130,26 +97,36 @@
             $('.widget-updated').html('');
         },
 
-        getDailyWord: function(){
-            google.load('feeds', '1', {callback : function(){
-                Widget.getFeed();
-            }});
+        getDailyQuote: function(){
+            return new Promise(function(resolve, reject){
+                google.load('feeds', '1', {callback : function(){
+                    return Widget.getFeed()
+                        .then(resolve)
+                        .catch(reject);
+                }});
+            });
         },
 
         getFeed: function() {
-            var feed = new google.feeds.Feed("http://unmotparjour.fr/feed/");
-            feed.load(function (result) {
-                if (!result.error) {
-                    $('.widget-header').html(
-                        '<h1>Mot du jour</h1>'
-                    );
-                    $('.widget-content').html(
-                        '<p>'+result.feed.entries[0].title+' : <br />'+result.feed.entries[0].content+'</p>'
-                    );
-                }
+            return new Promise(function(resolve, reject){
+                var feed = new google.feeds.Feed("http://unmotparjour.fr/feed/");
+                feed.load(function (result) {
+                    if (!result.error) {
+                        return resolve({
+                            title: result.feed.entries[0].title,
+                            content: result.feed.entries[0].content
+                        });
+                    }
+                    return reject(result.error);
+                });
             });
+
         }
     };
+
+    document.addEventListener('widget.ready', function(customEvent){
+        Widget.init(customEvent.detail);
+    });
 
 })();
 
